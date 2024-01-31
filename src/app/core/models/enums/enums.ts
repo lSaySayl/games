e0b1448185ccdc4768f0653e05512123bc763c9f
Camilo Ramirez <juacaramirezlo@gmail.com>
2024-01-30 22:11:23 -0500

    Close feature/001
    - Estableciendo archivos de configuración (Url-bases)
    
    - Creando interfaz y enums del formato de respuesta que entrega la api
    
    - Estableciendo servicio para la petición de los juegos
    
    - Consumiendo servicio para mostrar los datos en el front de forma inicial

---
+++ src/app/core/models/enums/enums.ts
@@ -0,0 +1,25 @@
+export enum Genre {
+    ActionRPG = "Action RPG",
+    Arpg = "ARPG",
+    BattleRoyale = "Battle Royale",
+    Card = "Card",
+    CardGame = "Card Game",
+    Fantasy = "Fantasy",
+    Fighting = "Fighting",
+    GenreMMORPG = " MMORPG",
+    MMORPG = "MMORPG",
+    Mmo = "MMO",
+    Mmoarpg = "MMOARPG",
+    Moba = "MOBA",
+    Racing = "Racing",
+    Shooter = "Shooter",
+    Social = "Social",
+    Sports = "Sports",
+    Strategy = "Strategy",
+}
+
+export enum Platform {
+    PCWindows = "PC (Windows)",
+    PCWindowsWebBrowser = "PC (Windows), Web Browser",
+    WebBrowser = "Web Browser",
+}
