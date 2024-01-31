ee15dba8b36f16e26c1f0951ae8b905113318f24
Camilo Ramirez <juacaramirezlo@gmail.com>
2024-01-30 23:09:06 -0500

    Restableciendo configuración por error en el enviroment

--- src/app/core/environments/environment.development.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-    production: false,
-    baseUrl: 'https://www.freetogame.com/api/games',
-    baseUrlRapid: 'https://free-to-play-games-database.p.rapidapi.com/api/games'
-};
